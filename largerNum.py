3835c61b3b6688963c6f97161690fb54bbbf87b7
Jose M. Rodriguez <jmrodriguez98842@gmail.com>
2019-03-28 14:12:27 -0500

    completed code

--- largerNum.py
+++ largerNum.py
@@ -2,13 +2,9 @@ firstNum = int(input('Enter first number: '))
 secondNum = int(input('Enter second number: '))
 
 if (firstNum > secondNum):
-<<<<<<< HEAD
     print ('Number 1 is bigger than Number 2')
-
-=======
-    print ('Num 1 is bigger than num 2')
 if (firstNum < secondNum):
     print('Num 2 is bigger than num 1')
 if (firstNum == secondNum):
     print('The numbers are equal!')
->>>>>>> firstNewFeature
+
